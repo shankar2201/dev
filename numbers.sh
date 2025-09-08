c20d21b88b221d9cd17b2df8583f0dc611606c4a
shankar2201 <dheeraj.aks03052018@gmail.com>
2025-09-08 11:57:47 +0530

    updated the script

--- numbers.sh
+++ numbers.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 read -p "enter the value ": value
-if [[ $value -lt "10 ] then
-  echo "value is single digit"
+if [ $value -lt "10" ] 
+then
+   echo "value is single digit"
 else
-  echo "value is double digit"
+   echo "value is double digit"
 fi
